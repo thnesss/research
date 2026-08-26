93a1e07a90f17249b7d55cf27eb33cf7966c056e
thnesss <Vane5sak@yandex.ru>
2026-08-26 14:38:27 +0300

    Update config.py

--- binance_market_data/config.py
+++ binance_market_data/config.py
@@ -1,6 +1,6 @@
 # Адреса WebSocket для разных рынков Binance.
 
 MARKET_URLS = {
-    "futures": "wss://fstream.binance.com/ws/",
-    "spot": "wss://stream.binance.com:9443/ws/"
+    "spot": "wss://stream.binance.com:9443/ws/",
+    "usdm_futures": "wss://fstream.binance.com/ws/"
 }
